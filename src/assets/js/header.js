cdc1406dc511ba3256b7a55d574cdfcf5b4a0a8c
Misha <serstoun001@gmail.com>
2020-01-12 17:44:12 +0300

    first commit

---
+++ src/assets/js/header.js
@@ -0,0 +1,34 @@
+
+const menu = document.querySelector('.nav');
+const burger = document.getElementById("burgerContainer");
+const header = document.querySelector(".header");
+
+window.addEventListener('scroll',fixedHeader);
+
+
+burger.onclick = function () {
+    this.classList.toggle("change");
+
+    if (menu.classList.contains('openMenu')) {
+        menu.classList.remove('openMenuAnimation');
+        setTimeout(() => {
+            menu.classList.remove('openMenu');
+        }, );
+    } else {
+        menu.classList.add('openMenu');
+        setTimeout(() => {
+            menu.classList.add('openMenuAnimation');
+        }, );
+    }
+};
+
+
+function fixedHeader() {
+    let windowRelativeTop = document.documentElement.getBoundingClientRect().top;
+
+    if (windowRelativeTop < 0) {
+        header.classList.add("headerFixed");
+    } else {
+        header.classList.remove("headerFixed");
+    }
+}
---
+++ src/index.js
@@ -0,0 +1,16 @@
+'use strict';
+import './assets/css/reset.css';
+import './assets/scss/layout.scss';
+import './assets/scss/styles.scss';
+import './assets/scss/teams.scss';
+
+import './assets/js/header.js';
+import './assets/js/team.js';
+
+
+
+
+
+
+
+
